7a44e2ebd497e8d663c8ffdeaea48e2de46ffc15
Nguyễn Công Hoan <hoannc2106@gmail.com>
2022-12-26 15:59:27 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { publicRoutes } from './routes';
-import { DefaultLayout } from './components/Layout';
 import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import DefaultLayout from '~/layouts';
+
 function App() {
     return (
         <Router>
             <div className="App">
                 <Routes>
-                    {publicRoutes.map((route, idx) => {
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
                         let Layout = DefaultLayout;
+
                         if (route.layout) {
                             Layout = route.layout;
                         } else if (route.layout === null) {
                             Layout = Fragment;
                         }
 
-                        const Page = route.component;
                         return (
                             <Route
-                                key={idx}
+                                key={index}
                                 path={route.path}
                                 element={
                                     <Layout>
